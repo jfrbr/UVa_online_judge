d713737d3686699f29933374d006e4faabf6aa73
juan fr <juan.franca@gmail.com>
2013-06-18 23:23:13 -0300

    11044 - Searching for Nessy

---
+++ 11044.cpp
@@ -0,0 +1,27 @@
+#include <cstring>
+#include <cstdio>
+
+using namespace std;
+
+int t,n,m;
+
+int main(){
+
+
+    scanf("%d\n",&t);
+
+    while (t--){
+        scanf("%d %d\n",&n,&m);
+        printf("%d\n",(n/3)*(m/3));
+
+    }
+
+
+
+    
+
+
+
+    return 0;
+
+}
